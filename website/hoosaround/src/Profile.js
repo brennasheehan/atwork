9a4d4e025318d9994c112e5b10e956888d6b3843
20rohrichtt <taylorrohrich@gmail.com>
2018-04-26 19:05:15 -0400

    code

--- website/hoosaround/src/Profile.js
+++ website/hoosaround/src/Profile.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
-
 class Profile extends Component {
   render() {
     return (
